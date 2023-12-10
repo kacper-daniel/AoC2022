4cdc83e39c594359c0b36351ea64f94a9d46d2db
Kacper Daniel <kacperd258@gmail.com>
2023-12-10 13:34:52 +0100

    day 8 done

--- 2023/day8.py
+++ 2023/day8.py
@@ -1,3 +1,5 @@
+import math
+
 data = open('inputs/day8_input.txt', 'r').read().split('\n')
 instructions = data[0]
 instructions = instructions.replace("L", "0").replace("R", "1")
@@ -25,23 +27,19 @@ while not_found_one:
 
 # part two
 
-not_found_two = True
 steps_two = 0
-curr = [x for x in destinations.keys() if x.endswith("A")]
-while not_found_two:
-    for i in range(len(instructions)):
-        where = int(instructions[i])
-        helper = []
-        steps_two += 1
-        endswith_z = 0
-        for el in curr:
-            helper.append(destinations[el][where].strip())
-            if helper[-1].endswith("Z"):
-                endswith_z += 1
-        curr = helper
-        if endswith_z == len(curr):
-            not_found_two = False
-            print(steps_two)
-            break
+cycles = []
+currents = [key for key in destinations.keys() if key[-1] == "A"]
+for c in currents:
+    cycle = 0
+    while c[-1] != "Z":
+        cycle += 1
+        for j in range(len(instructions)):
+            if c[-1] != "Z":
+                c = destinations[c.strip()][int(instructions[j])]
+            else:
+                break
+    cycles.append(cycle*len(instructions))
+steps_two = math.lcm(*cycles)
 
-# TODO: why its not working
+print(steps_two)
