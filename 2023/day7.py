f846de0cfaefd22e8703baa0b88515f25c54b67e
Kacper Daniel <kacperd258@gmail.com>
2023-12-12 18:55:20 +0100

    day 7 part two done

--- 2023/day7.py
+++ 2023/day7.py
@@ -46,6 +46,41 @@ for i in range(1, len(sorted_hands) + 1):
     output_one += sorted_hands[i-1][-1] * i
 print(output_one)
 
-# TODO: part two
-
 # part two
+
+def map_to_category_with_jokers(hand):
+    jokers = hand.count('J')
+    fixed_hand = [c for c in hand if c != 'J']
+    counted_chars = sorted(hand_to_dict(fixed_hand).values())
+    if not counted_chars:
+        counted_chars = [0] 
+    if counted_chars[-1] + jokers == 5:
+        return 1
+    if counted_chars[-1] + jokers == 4:
+        return 2
+    if counted_chars[-1] + jokers == 3 and counted_chars[-2] == 2:
+        return 3
+    if counted_chars[-1] + jokers == 3:
+        return 4
+    if counted_chars[-1] == 2 and (jokers or counted_chars[-2] == 2):
+        return 5
+    if counted_chars[-1] == 2 or jokers:
+        return 6
+    else:
+        return 7
+    
+def cards_as_int_with_jokers(hand: str) -> tuple:
+    ordered_labels = 'AKQT98765432J'
+    return (ordered_labels.index(card) for card in hand)
+
+sorted_hands_with_jokers = []
+for hand in hands_and_bids.keys():
+    encoded_hand = (map_to_category_with_jokers(hand), *cards_as_int_with_jokers(hand), hands_and_bids[hand])
+    sorted_hands_with_jokers.append(encoded_hand)
+
+sorted_hands_with_jokers.sort(reverse=True)
+
+output_two = 0
+for i in range(1, len(sorted_hands_with_jokers) + 1):
+    output_two += sorted_hands_with_jokers[i-1][-1] * i
+print(output_two)
