3fdce292a6dea7bae5ed1c45c4b382ce7a01b5a1
Kacper Daniel <kacperd258@gmail.com>
2023-12-12 19:15:32 +0100

    day 9 done

--- 2023/day9.py
+++ 2023/day9.py
@@ -7,21 +7,24 @@ def go_level_down(list):
     return diffs
 
 result = 0
+part_two = 0
 for line in data:
     parsed = [int(x) for x in line.split(" ")]
     helper = [parsed]
     one_below = go_level_down(parsed)
 
-    while not all(one_below) == 0:
+    while not all([x == 0 for x in one_below]):
         helper.append(one_below)
         one_below = go_level_down(one_below)
 
     for i in range(2, len(helper) + 1):
         helper[-i].append(helper[-i][-1] + helper[-i+1][-1])
+        helper[-i].insert(0, helper[-i][0] - helper[-i+1][0])
+
+    print(helper)
 
     result += helper[0][-1]
+    part_two += helper[0][0]
 
 print(result)
-
-
-# TODO: why its not working
+print(part_two)
